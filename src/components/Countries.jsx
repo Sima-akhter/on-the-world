0f19f9ad32a5b23c0647f0e30c5d8976b984ef7c
Sima-Akther <saimaislambdk@gmail.com>
2025-09-20 22:24:07 +0600

    added flag img

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,8 +1,26 @@
-import { use } from "react"
+import { use, useState } from "react"
 import Country from "./Country/Country";
 
 
+
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    const handleVisitedCountries =(country)=>{
+     console.log('handleVisitedCountries', country);
+     const newVisitedCountries = [...visitedCountries, country];
+     setVisitedCountries(newVisitedCountries)
+     
+    }
+
+    const handleVisitedFlag = (flag) =>{
+        // console.log('add click', flag)
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags)
+
+    }
+
 
     const countriesData = use(countriesPromise);
     
@@ -12,14 +30,33 @@ const Countries = ({countriesPromise}) => {
   return (
     <div>
       
-         <h1>Countries: {countries.length}</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 container mx-auto">
+        <div className="container mx-auto">
+        <h1 className="mt-10 p-5">Countries: {countries.length}</h1>
+        <h3>Total Country Visited: {visitedCountries.length}</h3>
+        <h3>Total Flags Visited: {visitedFlags.length}</h3>
+        <ol>
+            {
+                visitedCountries.map(country => <li key={country.cca3.cca3}>{country.name.common}</li>)
+            }
+        </ol>
+         
+         <div className="w-[150px] flex gap-5 p-5">
+            {
+                visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+            }
+         </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ">
         {
           countries.map(country => <Country
-            key={country.cca3.cca3}
-            country={country}></Country>)
+            key = {country.cca3.cca3}
+            country = {country}
+            handleVisitedCountries = {handleVisitedCountries}
+            handleVisitedFlag  = {handleVisitedFlag}
+            ></Country>)
         }
        </div>
+        </div>
 
 
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react"
 
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries, handleVisitedFlag }) => {
+
+    const [visited, setVisited] = useState(false)
+
     const heandleVisited = ()=>{
-        console.log('btn click');
-        
+        //system--------------1
+        // console.log('btn click');
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // system-------------2
+        // setVisited(visited ? false : true);
+
+        //system-------------3
+        setVisited(!visited);
+        handleVisitedCountries(country)
+
     }
     
     // console.log(country.area.area);
   return (
     <div>
-       <div className="border-2 rounded-2xl space-y-2 p-5">
+       <div className={`border-2 rounded-2xl space-y-2 p-5 ${visited && 'bg-gray-200'}`}>
          <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
         <h3 className="font-bold">Name: {country.name.common}</h3>
         <p>population: {country.population.population}</p>
         <p>Area: {country.area.area} 
         {country.area.area > 300000 ? ' Big country' : ' Small country'}
         </p>
-        <button onClick={heandleVisited} className="bg-blue-700 text-white py-2 px-4 rounded-md">Not Visited</button>
+       <div className="flex gap-5">
+         <button onClick={heandleVisited} className="bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-500">
+            {visited ? 'visited': 'Not visited'}
+        </button>
+        <button onClick={()=> {handleVisitedFlag (country?.flags?.flags?.png)} }className="bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-500">Add Visited Flag</button>
+       </div>
        </div>
     </div>
   )
